6642417609813d1298b1d830c00b6baa0d955122
mar1-eng <aouamimarouan@gmail.com>
2026-09-15 01:22:07 +0100

    Update some details

--- backend/index.js
+++ backend/index.js
@@ -48,8 +48,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   credentials: true
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
 app.use(cookieParser());
 
 // Attach Socket.io instance to req object
